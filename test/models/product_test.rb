811e10b00edcfda77493504b6ad0a6d179cf3b8a
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-10-22 17:57:12 +0200

    testing emails

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+    # set proudct out of stock
+    product.update(inventory_count: 0)
+    assert_emails 2 do
+      product.update(inventory_count: 48)
+    end
+  end
 end

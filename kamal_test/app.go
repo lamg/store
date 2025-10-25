b68f8953e65b6d062c298445c1d6a42792bc50ef
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-10-25 12:39:31 +0200

    refactoring

--- kamal_test/app.go
+++ kamal_test/app.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 )
 
+var port = ":80"
+var content = "Que bolón!💪"
+
 func main() {
 	http.HandleFunc("/", index)
-	fmt.Println("listening on port 80")
-	http.ListenAndServe(":80", nil)
+	fmt.Println("listening on port", port)
+	http.ListenAndServe(port, nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body><h1>Hello world!</h1></body></html>")
+	fmt.Fprintf(w, "<html><body><h1>%s</h1></body></html>", content)
 }
